5908c1082fc17cbd02bcc0ee35f1cd8abaef3d0e
simai <mullahashimsimai@gmail.com>
2023-11-20 08:04:28 -0500

    cleaned up the code

--- src/test/java/com/fetch/pages/Task_page.java
+++ src/test/java/com/fetch/pages/Task_page.java
@@ -27,58 +27,11 @@ public class Task_page {
     @FindBy(xpath = "//div[contains(text(), \"right\")]")
     public WebElement right_bowl;
 
-    @FindBy(id = "left_0")
-    public WebElement left_0;
-
-    @FindBy(id = "left_1")
-    public WebElement left_1;
-
-    @FindBy(id = "left_2")
-    public WebElement left_2;
-
-    @FindBy(id = "left_3")
-    public WebElement left_3;
-
     @FindBy(id = "left_4")
     public WebElement left_4;
 
-    @FindBy(id = "left_5")
-    public WebElement left_5;
-
-    @FindBy(id = "left_6")
-    public WebElement left_6;
-
-    @FindBy(id = "left_7")
-    public WebElement left_7;
-
-    @FindBy(id = "left_8")
-    public WebElement left_8;
-
-    @FindBy(id = "right_0")
-    public WebElement right_0;
-
-    @FindBy(id = "right_1")
-    public WebElement right_1;
-
-    @FindBy(id = "right_2")
-    public WebElement right_2;
-    @FindBy(id = "right_3")
-    public WebElement right_3;
-
-    @FindBy(id = "right_4")
-    public WebElement right_4;
-
     @FindBy(id = "right_5")
     public WebElement right_5;
-
-    @FindBy(id = "right_6")
-    public WebElement right_6;
-
-    @FindBy(id = "right_7")
-    public WebElement right_7;
-
-    @FindBy(id = "right_8")
-    public WebElement right_8;
 
     @FindBy(xpath = "//div[@class='game-info']//ol")
     public WebElement first_iterations_result;
@@ -94,5 +47,52 @@ public class Task_page {
 
     @FindBy(xpath = "//input[@class='square' and @data-side='right']")
     public List<WebElement> right_bowl_boxes;
+
+//    @FindBy(id = "left_5")
+//    public WebElement left_5;
+//
+//    @FindBy(id = "left_6")
+//    public WebElement left_6;
+//
+//    @FindBy(id = "left_7")
+//    public WebElement left_7;
+//
+//    @FindBy(id = "left_8")
+//    public WebElement left_8;
+//
+//    @FindBy(id = "right_0")
+//    public WebElement right_0;
+//
+//    @FindBy(id = "right_1")
+//    public WebElement right_1;
+//
+//    @FindBy(id = "right_2")
+//    public WebElement right_2;
+//    @FindBy(id = "right_3")
+//    public WebElement right_3;
+//
+//    @FindBy(id = "right_4")
+//    public WebElement right_4;
+
+//    @FindBy(id = "right_6")
+//    public WebElement right_6;
+//
+//    @FindBy(id = "right_7")
+//    public WebElement right_7;
+//
+//    @FindBy(id = "right_8")
+//    public WebElement right_8;
+
+//    @FindBy(id = "left_0")
+//    public WebElement left_0;
+//
+//    @FindBy(id = "left_1")
+//    public WebElement left_1;
+//
+//    @FindBy(id = "left_2")
+//    public WebElement left_2;
+//
+//    @FindBy(id = "left_3")
+//    public WebElement left_3;
 
 }
